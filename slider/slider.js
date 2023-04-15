e288952cd6e5e1a215a9d96c9d5c914520c849b1
Jose Imedio <jose.adan.imedio@gmail.com>
2023-04-15 13:35:23 +0200

    Add basic try of task 3

---
+++ slider/slider.js
@@ -0,0 +1,5 @@
+const btn_prev = document.getElementsByClassName("slider-prev");
+const btn_next = document.getElementsByClassName("slider-next");
+
+const element = document.get;
+element.innerHTML = "Hello world";
